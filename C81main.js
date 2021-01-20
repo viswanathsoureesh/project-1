15ef2368e5411dda43a00aed919b05e651717a57
viswanathsoureesh <73836557+viswanathsoureesh@users.noreply.github.com>
2021-01-20 18:56:32 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,40 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+colour="black"
+  canvas.addEventListener("mousedown",mycolour);
+  function mycolour(e){
+      mousex=e.clientX-canvas.offsetLeft;
+      mousey=e.clientY-canvas.offsetTop;
+      circle(   mousex,mousey)
+      canvas=document.getElementById("myCanvas");
+      ctx=canvas.getContext("2d");
+      colour="blue"
+  canvas.addEventListener("mousedown",mycolour);
+  function mycolour(e){
+      mousex=e.clientX-canvas.offsetLeft;
+      mousey=e.clientY-canvas.offsetTop;
+      circle(   mousex,mousey)
+      canvas=document.getElementById("myCanvas");
+      ctx=canvas.getContext("2d");
+      colour="red"
+  canvas.addEventListener("mousedown",mycolour);
+  function mycolour(e){
+      mousex=e.clientX-canvas.offsetLeft;
+      mousey=e.clientY-canvas.offsetTop;
+      circle(   mousex,mousey)
+      canvas=document.getElementById("myCanvas");
+      ctx=canvas.getContext("2d");
+      colour="yellow"
+  canvas.addEventListener("mousedown",mycolour);
+  function mycolour(e){
+      mousex=e.clientX-canvas.offsetLeft;
+      mousey=e.clientY-canvas.offsetTop;
+      circle(   mousex,mousey)
+      canvas=document.getElementById("myCanvas");
+      ctx=canvas.getContext("2d");
+      colour="green"
+  canvas.addEventListener("mousedown",mycolour);
+  function mycolour(e){
+      mousex=e.clientX-canvas.offsetLeft;
+      mousey=e.clientY-canvas.offsetTop;
+      circle(   mousex,mousey)
